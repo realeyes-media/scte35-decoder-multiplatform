9babfab392ad3fb22e4a57c8d6a3801c344ec0bd
Mike Patterson <mike.patterson@realeyes.com>
2021-01-16 15:36:40 -0700

    Fix github repo for packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/realeyes-media/scte35-decoder")
+            url = uri("https://maven.pkg.github.com/realeyes-media/scte35-decoder-multiplatform")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
