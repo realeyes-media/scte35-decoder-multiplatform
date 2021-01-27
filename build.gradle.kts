0ea61c51efe9043f1fcb100edbbd3b2fd947f352
Mostafa Ashraf <mostafa.ashraf@realeyes.com>
2021-01-27 17:53:02 +0200

    SCTE35-13 Pull request validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 
 group = "com.realeyes"
-version = "0.0.2"
+version = "0.0.1"
 
 publishing {
 
@@ -39,19 +39,12 @@ kotlin {
         publishLibraryVariants("release", "debug")
     }
 
-    ios {
-        binaries {
-            sharedLib {
-
-            }
-        }
-
-    }
+    ios ()
 
     cocoapods {
         summary = "CocoaPods stce35-decoder library"
-        homepage = "Link to a Kotlin/Native module homepage"
-        ios.deploymentTarget = "0.0.2"
+        homepage = "https://github.com/realeyes-media/scte35-decoder-multiplatform"
+        ios.deploymentTarget = "13.5"
     }
 
     js {
@@ -173,41 +166,41 @@ if (HostManager.hostIsMac) {
     }
 }
 
-val packForXcode by tasks.creating(Sync::class) {
-    val targetDir = File(buildDir, "xcode-frameworks")
-
-    /// selecting the right configuration for the iOS
-    /// framework depending on the environment
-    /// variables set by Xcode build
-    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-    val sdkName: String? = System.getenv("SDK_NAME")
-    val isiOSDevice = sdkName.orEmpty().startsWith("iphoneos")
-    val framework = kotlin.targets
-        .getByName<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget>(
-            if (isiOSDevice) {
-                "iosArm64"
-            } else {
-                "iosX64"
-            }
-        )
-        .binaries.getFramework(mode)
-    inputs.property("mode", mode)
-    dependsOn(framework.linkTask)
-
-    from({ framework.outputDirectory })
-    into(targetDir)
-
-    /// generate a helpful ./gradlew wrapper with embedded Java path
-    doLast {
-        val gradlew = File(targetDir, "gradlew")
-        gradlew.writeText(
-            "#!/bin/bash\n"
-                + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
-                + "cd '${rootProject.rootDir}'\n"
-                + "./gradlew \$@\n"
-        )
-        gradlew.setExecutable(true)
-    }
-}
-tasks.getByName("build").dependsOn(packForXcode)
+//val packForXcode by tasks.creating(Sync::class) {
+//    val targetDir = File(buildDir, "xcode-frameworks")
+//
+//    /// selecting the right configuration for the iOS
+//    /// framework depending on the environment
+//    /// variables set by Xcode build
+//    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+//    val sdkName: String? = System.getenv("SDK_NAME")
+//    val isiOSDevice = sdkName.orEmpty().startsWith("iphoneos")
+//    val framework = kotlin.targets
+//        .getByName<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget>(
+//            if (isiOSDevice) {
+//                "iosArm64"
+//            } else {
+//                "iosX64"
+//            }
+//        )
+//        .binaries.getFramework(mode)
+//    inputs.property("mode", mode)
+//    dependsOn(framework.linkTask)
+//
+//    from({ framework.outputDirectory })
+//    into(targetDir)
+//
+//    /// generate a helpful ./gradlew wrapper with embedded Java path
+//    doLast {
+//        val gradlew = File(targetDir, "gradlew")
+//        gradlew.writeText(
+//            "#!/bin/bash\n"
+//                + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
+//                + "cd '${rootProject.rootDir}'\n"
+//                + "./gradlew \$@\n"
+//        )
+//        gradlew.setExecutable(true)
+//    }
+//}
+//tasks.getByName("build").dependsOn(packForXcode)
 
